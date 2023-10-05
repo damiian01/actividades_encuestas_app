4f31bf1d0b942e450f31bce9c8da17f8a3f9f88f
damiian01 <damianaguero2023@gmail.com>
2023-10-05 00:33:19 -0300

    Actividad 4 React

---
+++ src/components/Encuestas.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from 'react';
+
+function Encuestas({ encuestas }) {
+  return (
+    <div>
+      <h2>Encuestas Disponibles</h2>
+      <ul>
+        {encuestas.map((encuesta) => (
+          <li key={encuesta.id}> {encuesta.pregunta}          
+            <ul>
+            {encuesta.opciones.map((opcion) => (
+              <li key={opcion}>{opcion}</li>
+            ))}
+            </ul>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default Encuestas;
